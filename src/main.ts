986a9a89ee081b7ace8a1f7c67694d07eb4cd6fc
ja0nz <mail@ja.nz>
2021-05-14 18:22:57 +0200

    moved implementation to rstream

--- src/main.ts
+++ src/main.ts
@@ -1,76 +1,72 @@
+import { fromDOMEvent, merge } from "@thi.ng/rstream";
 import * as tx from "@thi.ng/transducers";
 import { CodeJar } from "codejar";
 import Prism from "prismjs";
 import "prismjs/components/prism-typescript";
+Prism.manual = true; // no automatic highlighting
 
-const rnode = document.getElementById("r1c1");
-rnode?.addEventListener("mousedown", resizeByNeighborIndex(rnode.dataset));
-function resizeByNeighborIndex({ row = 0, col = 0 }) {
-  row = Number(row);
-  col = Number(col);
+// Grid settings
+const rnode = document.getElementById("r1c1") as HTMLElement;
+const activateEvent = fromDOMEvent(rnode, "mousedown")
+  .subscribe({
+    next(event: MouseEvent) {
+      event.target.style.zIndex = "1";
+      document.documentElement.style.setProperty(
+        "--toggle-capture-zone",
+        "block"
+      );
+    },
+  })
+  .transform(
+    tx.map((ev: MouseEvent) => ev.target.parentElement), // get hosting grid
+    tx.map(getComputedStyle), // get CSSStyleDeclaration
+    tx.multiplexObj({
+      rows: tx.pluck("gridTemplateRows"), //get rows: "1px 12px 12px"
+      columns: tx.pluck("gridTemplateColumns"), // get columns: "1px 12px 12px"
+    }),
+    tx.mapVals((x: string) => x.split("px")), // split "px" string: ["1" "12" "12" ""]
+    tx.mapVals((xs: string[]) => xs.filter((x) => x!!)), // filter empty split: ["1" "12" "12"]
+    tx.mapVals((xs: string[]) => xs.map(Number)) // map px values to numbers: [1 12 12]
+  );
 
-  return (ev: MouseEvent) => {
-    const resizetarget = ev.target as HTMLElement;
-    const gridparent = resizetarget.parentElement!;
+// deactivateEvent - set zIndex and toggle CSS
+merge({
+  src: [fromDOMEvent(rnode, "mouseup"), fromDOMEvent(rnode, "mouseleave")],
+}).subscribe({
+  next(event: MouseEvent) {
+    event.target.style.zIndex = "";
+    document.documentElement.style.setProperty("--toggle-capture-zone", "none");
+  },
+});
 
-    const xRulesToArray = tx.comp(
-      tx.mapcat((x: string) => x.split("px")),
-      tx.map((x: string) => Number(x)),
-      tx.filter((x: number) => x !== 0)
-    );
-    const rows = tx.step(tx.comp(tx.pluck("gridTemplateRows"), xRulesToArray))(
-      getComputedStyle(gridparent)
-    ) as number[];
+// resizeEvent (by mouseover) - alter grid parent Rows and Columns
+fromDOMEvent(rnode, "mousemove").subscribe({
+  next(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    // zIndex set to "1" if activated by activateStream
+    if (!!target.style.zIndex) {
+      const row = Number(target.dataset.row);
+      const col = Number(target.dataset.col);
+      const { columns, rows } = activateEvent.deref(); // IMPURE
 
-    const columns = tx.step(
-      tx.comp(tx.pluck("gridTemplateColumns"), xRulesToArray)
-    )(getComputedStyle(gridparent)) as number[];
-
-    // Resize block
-    function _resize(ev: MouseEvent) {
       // X --
-      columns[col - 1] = columns[col - 1] + ev.movementX;
-      columns[col + 1] = columns[col + 1] - ev.movementX;
-      gridparent.style.gridTemplateColumns = columns
-        .map((x) => String(x) + "px")
+      columns[col - 1] = columns[col - 1] + event.movementX;
+      columns[col + 1] = columns[col + 1] - event.movementX;
+      target.parentElement.style.gridTemplateColumns = columns
+        .map((x: number) => `${x}px`)
         .join(" ");
 
       // Y |
-      rows[row - 1] = rows[row - 1] + ev.movementY;
-      rows[row + 1] = rows[row + 1] - ev.movementY;
-      gridparent.style.gridTemplateRows = rows
-        .map((x) => String(x) + "px")
+      rows[row - 1] = rows[row - 1] + event.movementY;
+      rows[row + 1] = rows[row + 1] - event.movementY;
+      target.parentElement.style.gridTemplateRows = rows
+        .map((x: number) => `${x}px`)
         .join(" ");
     }
-
-    // Set up resize event
-    resizetarget.addEventListener("mousemove", _resize);
-    resizetarget.style.zIndex = "1";
-    document.documentElement.style.setProperty(
-      "--toggle-capture-zone",
-      "block"
-    );
-
-    // Cleanup block
-    function _cleanup() {
-      resizetarget.removeEventListener("mousemove", _resize);
-      resizetarget.style.zIndex = "";
-      document.documentElement.style.setProperty(
-        "--toggle-capture-zone",
-        "none"
-      );
-      // Remove self
-      resizetarget.removeEventListener("mouseup", _cleanup);
-      resizetarget.removeEventListener("mouseleave", _cleanup);
-    }
-
-    resizetarget.addEventListener("mouseup", _cleanup);
-    resizetarget.addEventListener("mouseleave", _cleanup);
-  };
-}
+  },
+});
 
 // Code Editor Section
-Prism.manual = true;
 const codeJarOpts = {
   tab: " ".repeat(4), // default is '\t'
 };
@@ -86,12 +82,7 @@ const codeJarHighlight = (editor: HTMLElement) => {
     "typescript"
   );
   const fragment = document.createRange().createContextualFragment(highlight);
-  // Cleanup
-  const range = document.createRange();
-  range.selectNodeContents(editor);
-  range.deleteContents();
-  // END Cleanup
-  editor.append(fragment);
+  editor.replaceChildren(fragment);
 };
 
 // Thing editor
